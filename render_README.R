839ea13ae7ca2d84f88152bfc145238889c45d80
Benjamin Elbers <elbersb@gmail.com>
2018-11-09 14:10:04 -0500

    update docs

--- render_README.R
+++ render_README.R
@@ -1,11 +1,10 @@
 if ("package:tibble" %in% search()) {
-    detach("package:tibble", unload=TRUE)
+    detach("package:tibble", unload = TRUE)
 }
 
 rmarkdown::render("README.Rmd",
                   output_format = rmarkdown::github_document(
-                      df_print = function(...) { print(..., row.names=FALSE) },
+                      df_print = function(...) { print(..., row.names = FALSE) },
                       html_preview = FALSE))
 
 pkgdown::build_site()
-
